0fe96e6bc5f376441cb586670a14f86d396bd8dc
Gael FERREIRA RODRIGUEZ <gael.ferreira-rodriguez@ens.uvsq.fr>
2023-02-17 23:30:21 +0100

    #

--- projetS2.py
+++ projetS2.py
@@ -1,10 +1,18 @@
 import tkinter as tk
 import random as rd
 import time
-### Création de la fenêtre ###
 
-HEIGHT = 600
-WIDTH = 820
+
+Liste_couleur = ['blue', 'green', 'white', 'yellow', 'purple', 'orange', 'red', 'black']
+Texte_couleur = ''
+for i in range(len(Liste_couleur)):
+    if i == len(Liste_couleur)-1:
+        Texte_couleur += Liste_couleur[i]+'.'
+    else:
+        Texte_couleur += Liste_couleur[i]+', '
+### Création de la fenêtre ###
+HEIGHT = 400
+WIDTH = 620
 colonne = 4
 fenetre = tk.Tk()
 fenetre.title("Mastermind")
@@ -13,74 +21,91 @@ canvas.grid()
 
 ### Création de la grille principale ###
 def grille():
-    x1, x2, y1, y2 = 10, 210, 100, 300
+    x1, x2, y1, y2 = 10, 150, 50, 200
     for loop in range(colonne):
         canvas.create_rectangle((x1,y1), (x2,y2), fill='gray77')
-        x1, x2 = x2, x2+200
+        x1, x2 = x2, x2+140
+    help = tk.Button(text="?", command=Help, font=("Helvetica", "5")).place(x=570, y=8)
 
 ### Création de la grille secondaire ###
 def grille2():
-    x1, x2, y1, y2 = 10, 40, 420, 450
+    x1, x2, y1, y2 = 10, 40, 270, 300
     for loop in range(colonne):
         canvas.create_rectangle((x1,y1), (x2,y2), fill='gray77')
         x1, x2 = x2, x2+30
 
 ### Fonctions pions bien plaçés et mal plaçés ###
-def bien_place(n):
-    x1, x2, y1, y2 = 10, 20, 350, 360
+def bien_place(n, essais):
+    if essais > 0:
+        canvas.delete("bienPlacé")
+    x1, x2, y1, y2 = 10, 20, 220, 230
     for loop in range(n):
-        canvas.create_oval((x1,y1),(x2,y2), fill='red')
+        pions = canvas.create_oval((x1,y1),(x2,y2), fill='red', tags="bienPlacé")
         x1, x2 = x1+30, x2+30
 
-def mal_place(n):
-    x1, x2, y1, y2 = 10, 20, 380, 390
+def mal_place(n, essais):
+    if essais > 0:
+        canvas.delete("malPlacé")
+    x1, x2, y1, y2 = 10, 20, 240, 250
     for loop in range(n):
-        canvas.create_oval((x1,y1),(x2,y2), fill='white')
+        canvas.create_oval((x1,y1),(x2,y2), fill='white', tags = "malPlacé")
         x1, x2 = x1+30, x2+30
+
+### Fonction Help ###
+def Help():
+    fenetre_help = tk.Tk()
+    fenetre.title("Aide pions")
+    canvas = tk.Canvas(fenetre_help, height=300, width=400, bg='papaya whip')
+    canvas.grid()
+    couleur = tk.Label(fenetre_help, text='Couleurs : ', font = ("helvetica", "8"), bg='papaya whip').place(x=10, y=20)
+    listeCouleurs = tk.Label(fenetre_help, text=Texte_couleur, font = ("helvetica", "8"), bg='papaya whip').place(x=70, y=20)
+    canvas.create_oval((10,95),(20,105), fill='red')
+    canvas.create_oval((10,195),(20,205), fill='white')
+    bienPlace = tk.Label(fenetre_help, text=": correspond aux pions BIEN plaçés.", font = ("helvetica", "10"), bg='papaya whip').place(x=25, y=90)
+    malPlace = tk.Label(fenetre_help, text=": correspond aux pions MAL plaçés.", font = ("helvetica", "10"), bg='papaya whip').place(x=25, y=190)
 
 ### Fonction victoire ###
 def Victoire():
     canvas.delete("all")
-    victoire = tk.Label(fenetre, text="Vous avez gagné(e)",font = ("helvetica", "60"))
+    victoire = tk.Label(fenetre, text="Vous avez gagné(e) !", font = ("helvetica", "50"), bg='papaya whip')
     victoire.grid(column=0, row=0, pady=50)
 
 ### Fonction défaite ###
 def Defaite():
     canvas.delete("all")
-    victoire = tk.Label(fenetre, text="Vous avez perdu(e)",font = ("helvetica", "60"))
-    victoire.grid(column=0, row=0, pady=50)
+    defaite = tk.Label(fenetre, text="Vous avez perdu(e)",font = ("helvetica", "50"), bg='papaya whip')
+    defaite.grid(column=0, row=0, pady=50)
     
 ### Fonction un joueur ###
-Liste_couleur = ['blue', 'green', 'white', 'yellow', 'purple', 'orange', 'red', 'black']
-
 def unJoueur():
     grille()
     grille2()
     couleurs_grille = []
-    essais = 0
-    bienPlace = 0
-    malPlace = 0
+    essais = 10
     couleurs_joueur = []
     pions = [0, 0, 0, 0]
     for i in range(4):
         couleurs_grille.append(Liste_couleur[rd.randint(0,7)])
     for loop in range(11):
-        x1, x2, y1, y2 = 60, 160, 150, 250
-        x3, x4, y3, y4 = 20, 30, 430, 440
+        essais_restants = tk.Label(text="essais restant(s) : "+str(essais), font = ("helvetica", "8"), bg='papaya whip').place(x=10,y=10)
+        x1, x2, y1, y2 = 50, 110, 100, 150
+        x3, x4, y3, y4 = 20, 30, 280, 290
         for i in range(colonne):
             couleur = str(input('Choisissez la couleur '+ str(i+1) +'(en anglais et en minuscule) : '))
             while not (couleur in Liste_couleur):
                 couleur = str(input('Choisissez une autre couleur (en anglais et en minuscule) ou vérifier votre écriture : '))
             couleurs_joueur.append(couleur)
             pions[i] = canvas.create_oval((x1,y1),(x2,y2), fill=couleurs_joueur[i])
-            x1, x2 = x1+200, x2+200
+            x1, x2 = x1+140, x2+140
         if couleurs_joueur == couleurs_grille:
             Victoire()
             continue
-        elif essais > 10:
+        elif essais == 0:
             Defaite()
             continue
         else:
+            bienPlace = 0
+            malPlace = 0
             for k in range(4):
                 if (couleurs_joueur[k] == couleurs_grille[k]):
                     bienPlace += 1
@@ -91,15 +116,16 @@ def unJoueur():
         for m in range(4):
             canvas.create_oval((x3,y3),(x4,y4), fill=couleurs_joueur[m])
             x3, x4 = x3+30, x4+30
-        bien_place(bienPlace)
-        mal_place(malPlace)
+        bien_place(bienPlace, essais)
+        mal_place(malPlace, essais)
         couleurs_joueur = []
-        essais += 1
+        essais -= 1
     
     
 
-### Fonction un joueur ###
+
 
 
 unJoueur()
+fenetre_help.mainloop()
 fenetre.mainloop()
